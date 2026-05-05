c8ead780867e06997031b0d992f9760d032cd704
gijsbertvandeweg-stack <gijsbertvandeweg@gmail.com>
2026-05-05 13:36:41 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'prijscalculatie-v1';
+const ASSETS = [
+  '/prijscalculatie.html',
+  '/manifest.json',
+  '/sw.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS).catch(() => {
+        console.log('Cache addAll failed, continuing without some assets');
+      });
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/prijscalculatie.html');
+      });
+    })
+  );
+});
